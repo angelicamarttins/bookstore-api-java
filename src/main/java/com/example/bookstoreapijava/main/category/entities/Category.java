0ac97baeb401b0ad8371ac838219d63f3bf4e834
Angélica Martins <marttinsangelica@gmail.com>
2024-04-16 21:45:28 -0300

    Refactor: Migrate Category class to Lombok

--- src/main/java/com/example/bookstoreapijava/main/category/entities/Category.java
+++ src/main/java/com/example/bookstoreapijava/main/category/entities/Category.java
@@ -1,34 +1,38 @@
 package com.example.bookstoreapijava.main.category.entities;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.UuidGenerator;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Category {
 
   @Column(name = "category_id")
   @Id
+  @NonNull
   private UUID categoryId;
 
   @Column(name = "category_name", unique = true)
+  @NonNull
   @NotNull(message = "O nome da categoria não pode ser nulo")
   @NotEmpty(message = "O nome da categoria não pode estar vazio")
   @Size(min = 1, max = 500, message = "O nome da categoria deve conter entre 1 e 500 caracteres")
   private String categoryName;
 
   @Column(name = "created_at")
+  @NonNull
   private LocalDateTime createdAt;
 
   @Column(name = "updated_at")
@@ -37,64 +41,12 @@ public class Category {
   @Column(name = "inactivated_at")
   private LocalDateTime inactivatedAt;
 
-  @JsonCreator
-  public Category() {
-  }
-
   public Category(
       @JsonProperty("categoryId") UUID categoryId,
       @JsonProperty("categoryName") String categoryName
   ) {
     this.categoryId = categoryId;
     this.categoryName = categoryName;
-  }
-
-  public Category(UUID categoryId, String categoryName, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime inactivatedAt) {
-    this.categoryId = categoryId;
-    this.categoryName = categoryName;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.inactivatedAt = inactivatedAt;
-  }
-
-  public UUID getCategoryId() {
-    return categoryId;
-  }
-
-  public void setCategoryId(UUID categoryId) {
-    this.categoryId = categoryId;
-  }
-
-  public String getCategoryName() {
-    return categoryName;
-  }
-
-  public void setCategoryName(String categoryName) {
-    this.categoryName = categoryName;
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public LocalDateTime getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(LocalDateTime updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
-  public LocalDateTime getInactivatedAt() {
-    return inactivatedAt;
-  }
-
-  public void setInactivatedAt(LocalDateTime inactivatedAt) {
-    this.inactivatedAt = inactivatedAt;
   }
 
   @PrePersist
@@ -113,35 +65,4 @@ public class Category {
     this.updatedAt = LocalDateTime.now();
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Category category)) return false;
-    return Objects.equals(getCategoryId(), category.getCategoryId()) &&
-        Objects.equals(getCategoryName(), category.getCategoryName()) &&
-        Objects.equals(getCreatedAt(), category.getCreatedAt()) &&
-        Objects.equals(getUpdatedAt(), category.getUpdatedAt()) &&
-        Objects.equals(getInactivatedAt(), category.getInactivatedAt());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(
-        getCategoryId(),
-        getCategoryName(),
-        getCreatedAt(),
-        getUpdatedAt(),
-        getInactivatedAt());
-  }
-
-  @Override
-  public String toString() {
-    return "Category{" +
-        "categoryId=" + categoryId +
-        ", categoryName='" + categoryName + '\'' +
-        ", createdAt=" + createdAt +
-        ", updatedAt=" + updatedAt +
-        ", inactivatedAt=" + inactivatedAt +
-        '}';
-  }
 }
