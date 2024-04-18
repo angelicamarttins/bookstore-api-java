d8eb9ee6370c6756b6a9170322c0cf0933af4b43
Angélica Martins <marttinsangelica@gmail.com>
2024-04-18 16:01:24 -0300

    Test: Add createCategoryCreatedVO in CategoryCreatedVOProvider

---
+++ src/test/java/com/example/bookstoreapijava/providers/CategoryCreatedVOProvider.java
@@ -0,0 +1,18 @@
+package com.example.bookstoreapijava.providers;
+
+import com.example.bookstoreapijava.main.category.data.vo.CategoryCreatedVO;
+import com.example.bookstoreapijava.main.category.entities.Category;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CategoryCreatedVOProvider {
+
+  public CategoryCreatedVO createCategoryCreatedVO(Category category) throws URISyntaxException {
+    URI uri =
+        new URI("http://localhost:8080/category/" + category.getCategoryId());
+
+    return new CategoryCreatedVO(category, uri);
+  }
+
+}
