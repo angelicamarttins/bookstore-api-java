d07c0efeadcc45986be65b8898464474d42f0e2f
Angélica Martins <marttinsangelica@gmail.com>
2024-05-04 15:54:15 -0300

    Chore: Rename Application class

--- src/main/java/com/example/bookstoreapijava/Application.java
+++ src/main/java/com/example/bookstoreapijava/Application.java
@@ -2,15 +2,13 @@ package com.example.bookstoreapijava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 
-@EnableKafka
 @SpringBootApplication
-public class BookstoreApiJavaApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(BookstoreApiJavaApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
--- src/test/java/com/example/bookstoreapijava/ApplicationTests.java
+++ src/test/java/com/example/bookstoreapijava/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookstoreApiJavaApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
