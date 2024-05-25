03ba75b9906cf9e7a31b84b3043ee269c178aca1
Angélica Martins <marttinsangelica@gmail.com>
2024-05-25 19:37:37 -0300

    Feature: Create pageable findAllById in BookRepository

--- src/main/java/com/example/bookstoreapijava/main/repositories/BookRepository.java
+++ src/main/java/com/example/bookstoreapijava/main/repositories/BookRepository.java
@@ -4,10 +4,14 @@ package com.example.bookstoreapijava.main.repositories;
 import com.example.bookstoreapijava.main.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
   Optional<Book> findBookByIsbn(String isbn);
+
+  List<Book> findAllById(UUID bookId, Pageable pageable);
 }
