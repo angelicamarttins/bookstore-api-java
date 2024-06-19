71f3036c28eb978c46f36ddda8ea387dbd690ae4
Angélica Martins <marttinsangelica@gmail.com>
2024-06-19 16:17:04 -0300

    Feature: Create findAllActiveCategories in CategoryRepository

--- src/main/java/com/example/bookstoreapijava/main/repositories/CategoryRepository.java
+++ src/main/java/com/example/bookstoreapijava/main/repositories/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.bookstoreapijava.main.repositories;
 
 import com.example.bookstoreapijava.main.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +13,9 @@ import java.util.UUID;
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
   Optional<Category> getBySanitizedCategoryName(String sanitizedCategoryName);
+
+  @Query("SELECT c FROM Category c WHERE c.inactivatedAt IS NULL")
+  Page<Category> findAllActiveCategories(Pageable pageable);
 
   @Modifying
   @Query("UPDATE Category SET inactivatedAt = NULL " +
